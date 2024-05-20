57f80b832a86389a51c8b09671489bffe175d0d4
Stelian Grigore Cardos <scardos@talentingsoftware.com>
2024-05-20 19:15:59 +0300

    Styled the error component

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   standalone: true,
   imports: [],
   templateUrl: './error.component.html',
-  styleUrl: './error.component.css'
+  styleUrl: './error.component.css',
 })
 export class ErrorComponent {
+  constructor(private router: Router) {}
 
+  goBack(): void {
+    this.router.navigate(['']);
+  }
 }
