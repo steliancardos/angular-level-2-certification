29aaaecfd40fd0c009f880cc2de4d7f737cfbffa
Stelian Grigore Cardos <scardos@talentingsoftware.com>
2024-05-21 16:47:55 +0300

    Added special setting for worker registration, needed for github pages

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,19 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { setupWorker } from 'msw/browser';
 import { mockHandlers } from './mocks';
-
-setupWorker(...mockHandlers)
-  .start({
-    serviceWorker: {
-      url: '/angular-level-2-certification/mockServiceWorker.js',
-    },
-  })
-  .then(() => bootstrapApplication(AppComponent, appConfig))
-  .catch((err) => console.error(err));
+import { isDevMode } from '@angular/core';
+if (isDevMode()) {
+  setupWorker(...mockHandlers)
+    .start()
+    .then(() => bootstrapApplication(AppComponent, appConfig))
+    .catch((err) => console.error(err));
+} else {
+  setupWorker(...mockHandlers)
+    .start({
+      serviceWorker: {
+        url: '/angular-level-2-certification/mockServiceWorker.js',
+      },
+    })
+    .then(() => bootstrapApplication(AppComponent, appConfig))
+    .catch((err) => console.error(err));
+}
