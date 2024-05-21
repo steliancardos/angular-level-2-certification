bda4fffd22ed54cbbc9ac986c7ba64f18acec2b2
Stelian Grigore Cardos <scardos@talentingsoftware.com>
2024-05-21 14:57:49 +0300

    new settings for the worker

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {setupWorker} from 'msw/browser';
+import { setupWorker } from 'msw/browser';
 import { mockHandlers } from './mocks';
 
-setupWorker(...mockHandlers).start()
+setupWorker(...mockHandlers)
+  .start({
+    serviceWorker: {
+      url: '/angular-level-2-certification/mockServiceWorker.js',
+    },
+  })
   .then(() => bootstrapApplication(AppComponent, appConfig))
   .catch((err) => console.error(err));
-
